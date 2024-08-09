0db399052dcd0d9e8924769042ee2f4740bc922e
vaishnavi4002 <117152991+vaishnavi4002@users.noreply.github.com>
2024-08-09 11:58:41 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  base: "/Little-Lemon/",
+  base: "/home/",
   plugins: [
     react(),
     visualizer({
